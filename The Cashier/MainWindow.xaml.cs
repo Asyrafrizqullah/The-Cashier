44128616f6db5510c9d3f6e58d6db46111db7fec
Muh Asyraf Rizqullah <muh.rizqullah@students.amikom.ac.id>
2020-10-17 16:43:11 +0800

    Add project files.

---
+++ The Cashier/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace The_Cashier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            ListBox.ItemsSource = calculator.getListItem();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string title = ItemNameBox.Text;
+            int quantity = int.Parse(QuantityBox.Text);
+            string type = TypeBox.Text;
+            double price = double.Parse(PriceBox.Text);
+
+            Item item = new Item(new Random().Next(), title, quantity, price,price, type);
+            calculator.addItem(item);
+            double total = calculator.getTotal();
+
+            TotalLabel.Content = string.Format("Rp. {0}", total);
+            ListBox.Items.Refresh();
+        }
+    }
+}
